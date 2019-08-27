342399d1fd759f4bc57ebc1913ad681ac20b61f0
mahendrathapa <mahendra@fusemachines.com>
2019-08-27 19:47:21 +0545

    Extract train_set and val_set csv file.
    Extract train_set and val_set from train_image_path and
    val_image_path csv file.
    
    Label 1 = Positive = Abnormal
    Label 0 = Negative = Normal

---
+++ src/data/convert_data_set.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pathlib import Path
+
+
+def convert_data_set(data_path):
+    df = pd.read_csv(data_path, header=None)
+    df.columns = ['image_path']
+    df['label'] = df['image_path'].apply(lambda x: 1 if 'positive' in x else 0)
+    return df
+
+
+if __name__ == '__main__':
+    dir_path = Path('data/MURA-v1.1/')
+
+    train_set = convert_data_set(dir_path / 'train_image_paths.csv')
+    train_set.to_csv(dir_path / 'train_data.csv', index=False)
+
+    val_set = convert_data_set(dir_path / 'valid_image_paths.csv')
+    val_set.to_csv(dir_path / 'val_data.csv', index=False)
---
+++ src/main.py
@@ -0,0 +1,3 @@
+from data import load_data_set
+
+load_data_set.get_data_set('data/MURA-v1.1', 'train')
