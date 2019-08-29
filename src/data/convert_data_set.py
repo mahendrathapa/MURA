a7a4e3b0d32d425846e6f73375f36ca3cc2826f4
mahendrathapa <mahendra@fusemachines.com>
2019-08-29 21:03:59 +0545

    Update .gitignore and convert_data_set
    Update .gitignore to ignore .vscode
    
    Update convert_data_set to save the train_data as train_positive_data
    and train_negative_data.

--- src/data/convert_data_set.py
+++ src/data/convert_data_set.py
@@ -13,7 +13,14 @@ if __name__ == '__main__':
     dir_path = Path('data/MURA-v1.1/')
 
     train_set = convert_data_set(dir_path / 'train_image_paths.csv')
-    train_set.to_csv(dir_path / 'train_data.csv', index=False)
+
+    train_positive_set = train_set[train_set['label'] == 1]
+    train_negative_set = train_set[train_set['label'] == 0]
+
+    train_positive_set.to_csv(
+        dir_path / 'train_positive_data.csv', index=False)
+    train_negative_set.to_csv(
+        dir_path / 'train_negative_data.csv', index=False)
 
     val_set = convert_data_set(dir_path / 'valid_image_paths.csv')
     val_set.to_csv(dir_path / 'val_data.csv', index=False)
