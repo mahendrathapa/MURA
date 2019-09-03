9950afa381da4da6cbbeabe1e763705dee10e3e3
miranghimire-fm <miran@fusemachines.com>
2019-09-03 16:54:23 +0545

    Refactor config

--- src/config/config.py
+++ src/config/config.py
@@ -2,29 +2,51 @@ import os
 
 
 class Config:
-    ROOT_PATH = os.path.join(os.getcwd(), "src")
-    OUTPUT_ROOT_PATH = os.path.join(ROOT_PATH, "out")
-    DATA_ROOT_PATH = os.path.join(ROOT_PATH, "data")
-
-    TRAIN_DATA_DIR = os.path.join(DATA_ROOT_PATH, "train")
-    VAL_DATA_DIR = os.path.join(DATA_ROOT_PATH, "val")
-    # TEST_DATA_DIR = os.path.join(DATA_ROOT_PATH, "test")
-
+    ROOT_DIR = "src"
+    BASE_DATA_DIR = "data"
+    DATA_DIR = "MURA-v1.1"
+    OUT_DIR = "out"
+    TRAIN_DIR = "train"
+    VAL_DIR = "val"
     TRAIN_DATA_CSV = "train_data.csv"
+    TRAIN_POS_DATA_CSV = "train_positive_data.csv"
+    TRAIN_NEG_DATA_CSV = "train_negative_data.csv"
     VAL_DATA_CSV = "val_data.csv"
-    # TEST_DATA_CSV = "test_data.csv"
-
     JSON_CONFIG = "config.json"
     JSON_INDENT = 4
-
     ACCEPTED_DATA_TYPES = ["/*.png", "/*.jpg"]
     # IGNORE_FILES = ["edge", "annotated"]
+
+    ROOT_PATH = os.path.join(os.getcwd(), ROOT_DIR)
+    OUTPUT_ROOT_PATH = os.path.join(ROOT_PATH, OUT_DIR)
+    DATA_ROOT_PATH = os.path.join(ROOT_PATH, BASE_DATA_DIR)
+
+    TRAIN_DATA_DIR = os.path.join(DATA_ROOT_PATH, TRAIN_DIR)
+    VAL_DATA_DIR = os.path.join(DATA_ROOT_PATH, VAL_DIR)
+    # TEST_DATA_DIR = os.path.join(DATA_ROOT_PATH, TEST_DIR)
+
+    TRAIN_POS_DATA_CSV = os.path.join(
+            DATA_ROOT_PATH,
+            DATA_DIR,
+            TRAIN_POS_DATA_CSV
+    )
+    TRAIN_NEG_DATA_CSV = os.path.join(
+            DATA_ROOT_PATH,
+            DATA_DIR,
+            TRAIN_NEG_DATA_CSV
+    )
+    VAL_DATA_CSV = os.path.join(
+            DATA_ROOT_PATH,
+            DATA_DIR,
+            VAL_DATA_CSV
+    )
+    # TEST_DATA_CSV = "test_data.csv"
 
     def __init__(self, train_batch_size, val_batch_size, test_batch_size,
                  train_num_workers, val_num_workers, test_num_workers,
                  train_shuffle=True, val_shuffle=True, test_shuffle=True,
                  learning_rate=0.0001, epoch=100, model_dump_gap=2,
-                 cutoff_thersh=0.5):
+                 sampling_ratio=1.0, cutoff_thersh=0.5):
 
         self.TRAIN_BATCH_SIZE = train_batch_size
         self.VAL_BATCH_SIZE = val_batch_size
@@ -38,7 +60,7 @@ class Config:
         self.VAL_SHUFFLE = val_shuffle
         self.TEST_SHUFFLE = test_shuffle
 
-        self.SAMPLING_RATIO = 1.0
+        self.SAMPLING_RATIO = sampling_ratio
 
         self.learning_rate = learning_rate
         self.epoch = epoch
@@ -54,21 +76,21 @@ class LocalConfig(Config):
             train_batch_size=1,
             test_batch_size=1,
             val_batch_size=1,
-            # train_num_workers=os.cpu_count(),
             train_num_workers=1,
-            # val_num_workers=os.cpu_count(),
             val_num_workers=1,
-            test_num_workers=os.cpu_count()
+            test_num_workers=1,
+            model_dump_gap=2
         )
 
 
 class ServerConfig(Config):
     def __init__(self):
         super().__init__(
-            train_batch_size=1,
-            test_batch_size=1,
-            val_batch_size=1,
+            train_batch_size=16,
+            test_batch_size=16,
+            val_batch_size=16,
             train_num_workers=os.cpu_count(),
             val_num_workers=os.cpu_count(),
-            test_num_workers=os.cpu_count()
+            test_num_workers=os.cpu_count(),
+            model_dump_gap=100
         )
