cb875d83fd57562b459a99ca16bbbd40ea644f62
miranghimire-fm <miran@fusemachines.com>
2019-09-07 19:59:28 +0545

    Change densenet to use from 169 to 121

--- src/constants.py
+++ src/constants.py
@@ -2,16 +2,20 @@ from collections import OrderedDict
 
 
 class Constants:
-    DENSE_BLOCK_STRUCTURE = (6, 12, 32, 32)
+    DENSE_NET = {
+            "densenet-169": (6, 12, 32, 32),
+            "densenet-121": (6, 12, 24, 16)
+    }
+    DENSE_BLOCK_STRUCTURE = DENSE_NET["densenet-121"]
 
     IMAGE_SIZE = 320
 
-    NORMALIZE = False
+    NORMALIZE = True
 
     TRANSFORMATIONS = ["flip", "jitter", "rotate", "compose"]
 
     COMPOSE_PROBABILITIES = OrderedDict({
         "flip": 0.5,
-        "jitter": 0,
+        "jitter": 0.5,
         "rotate": 0.5
     })
