caf8f1ce9af5f09eab4f0aba80cab4ff348590d5
mahendrathapa <mahendra@fusemachines.com>
2019-09-10 19:31:41 +0545

    Add '/predict' end point to predict the given image with CAM.

--- src/constants.py
+++ src/constants.py
@@ -3,15 +3,15 @@ from collections import OrderedDict
 
 class Constants:
     DENSE_NET = {
-            "densenet-169": (6, 12, 32, 32),
-            "densenet-121": (6, 12, 24, 16)
+        "densenet-169": (6, 12, 32, 32),
+        "densenet-121": (6, 12, 24, 16)
     }
     DENSE_BLOCK_STRUCTURE = DENSE_NET["densenet-121"]
 
     IMAGE_SIZE = 320
 
     NORMALIZE = True
-    
+
     GLOBAL_MEAN = 52.60
 
     LR_DECAY = {"no_of_epochs": 4, "factor": 0.1}
@@ -23,3 +23,11 @@ class Constants:
         "jitter": 0.5,
         "rotate": 0.5
     })
+
+    # For CAM
+    FINAL_CONV_LAYER = "relu"
+    FC_LAYER = "fc1"
+
+    # For prediction
+    RUN_ID = ""
+    MODEL_NAME = ""
