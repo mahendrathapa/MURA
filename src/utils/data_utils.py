Fix normalizing the image when pretrained mode is on When pretrained mode is on, now image won't be normalized by subtracting the global mean as the normalization procedure of how image was pretrained will be on.

src/utils/data_utils.py
@@ -44,7 +44,7 @@ def get_image(image_path, transforms=None, unsqueeze_dim=1):
     image = np.array(image)
     image = image.astype('float')
 
-    if Constants.NORMALIZE:
+    if Constants.NORMALIZE and not Constants.PRETRAINED:
         image = image - Constants.GLOBAL_MEAN
 
     if Constants.PRETRAINED: